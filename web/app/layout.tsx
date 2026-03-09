71da8869d79698c21b981a06e591ce9fa3007012
root <root@vmi3097009.contaboserver.net>
2026-03-09 17:53:52 +0100

    fix: remove duplicate Help button - LinaAssistantDock removed, HelpCenterButton is the single Help button

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -311,7 +311,6 @@ export default function RootLayout({
         <Providers>
           <PlatformOsTag />
           <BackButton />
-          <LinaAssistantDock />
           <AppShell />
           <PushNotifManager />
           <HelpCenterButton />
